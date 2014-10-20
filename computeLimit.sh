471d205d7242def4d0d4b92a33d9a66451e83771
GuillelmoGomezCeballos <ceballos@cern.ch>
2014-10-20 15:41:20 +0200

    new changes

--- computeLimit.sh
+++ computeLimit.sh
@@ -17,13 +17,21 @@ mkdir -p /tmp/$USER/${seed}/;
 cd /tmp/$USER/${seed}/;
 
 if [ $3 = "exp" ]; then
-  echo "combine -M Asymptotic ${workspace} --newExpected=1 --expectSignal=1 -t -1 -m ${mass} -n hWW${tag} | tee limit${tag}.txt";
-        combine -M Asymptotic ${workspace} --newExpected=1 --expectSignal=1 -t -1 -m ${mass} -n hWW${tag} | tee limit${tag}.txt;
-  mv higgsCombinehWW${tag}.AsymptoticExp.mH${mass}.root limitExp${tag}.txt $baseDir/$mass/;
+  # --expectSignal=1
+  echo "combine -M Asymptotic ${workspace} --newExpected=1 -t -1 -m ${mass} -n hWWExp${tag} -s ${seed} | tee limitExp${tag}.txt";
+        combine -M Asymptotic ${workspace} --newExpected=1 -t -1 -m ${mass} -n hWWExp${tag} -s ${seed} | tee limitExp${tag}.txt;
+  mv limitExp${tag}.txt $baseDir/$mass/;
+  mv higgsCombinehWWExp${tag}.Asymptotic.mH${mass}.${seed}.root $baseDir/$mass/higgsCombinehWWExp${tag}.Asymptotic.mH${mass}.root;
 elif [ $3 = "obs" ]; then
-  echo "combine -M Asymptotic ${workspace} --newExpected=1 -m ${mass} -n hWW${tag} | tee limit${tag}.txt";
-        combine -M Asymptotic ${workspace} --newExpected=1 -m ${mass} -n hWW${tag} | tee limit${tag}.txt;
-  mv higgsCombinehWW${tag}.Asymptotic.mH${mass}.root limit${tag}.txt $baseDir/$mass/;
+  echo "combine -M Asymptotic ${workspace} --newExpected=1 -m ${mass} -n hWW${tag} -s ${seed} | tee limit${tag}.txt";
+        combine -M Asymptotic ${workspace} --newExpected=1 -m ${mass} -n hWW${tag} -s ${seed} | tee limit${tag}.txt;
+  mv limit${tag}.txt $baseDir/$mass/;
+  mv higgsCombinehWW${tag}.Asymptotic.mH${mass}.${seed}.root $baseDir/$mass/higgsCombinehWW${tag}.Asymptotic.mH${mass}.root;
+elif [ $3 = "toy" ]; then
+    combine -M HybridNew --frequentist $workspace -m ${mass} -n Toy${tag} --saveToys --fullBToys --saveHybridResult -T 10000 -s ${seed}                        | tee limitToyA${tag}.txt;
+    combine -M HybridNew --frequentist $workspace -m ${mass} -n Toy${tag} --grid=higgsCombineToy${tag}.HybridNew.mH${mass}.${seed}.root --expectedFromGrid=0.5 | tee limitToyB${tag}.txt;
+    combine -M HybridNew --frequentist $workspace -m ${mass} -n Toy${tag} --grid=higgsCombineToy${tag}.HybridNew.mH${mass}.${seed}.root                        | tee limitToyC${tag}.txt;
+    mv higgsCombineToy${tag}.HybridNew.mH${mass}.quant0.500.root higgsCombineToy${tag}.HybridNew.mH${mass}.root limitToy?${tag}.txt $baseDir/$mass/;
 else
     echo "don't know what to do for $3";
 fi
--- computeSignificance.sh
+++ computeSignificance.sh
@@ -15,13 +15,20 @@ mkdir -p /tmp/$USER/${seed}/
 cd /tmp/$USER/${seed}/
 
 if [ $3 = "exp" ]; then
-    echo "combine $workspace -M ProfileLikelihood --expectSignal=1 --significance -t -1 -m ${mass} -n Expected${tag} | tee expected${tag}.txt";
-          combine $workspace -M ProfileLikelihood --expectSignal=1 --significance -t -1 -m ${mass} -n Expected${tag} | tee expected${tag}.txt;
-    mv higgsCombineExpected${tag}.ProfileLikelihood.mH${mass}.root expected${tag}.txt $baseDir/$mass/;
+    echo "combine $workspace -M ProfileLikelihood --significance --expectSignal=1 -t -1 -m ${mass} -n Expected${tag} -s ${seed} | tee expected${tag}.txt";
+          combine $workspace -M ProfileLikelihood --significance --expectSignal=1 -t -1 -m ${mass} -n Expected${tag} -s ${seed} | tee expected${tag}.txt;
+    mv expected${tag}.txt $baseDir/$mass/;
+    mv higgsCombineExpected${tag}.ProfileLikelihood.mH${mass}.${seed}.root $baseDir/$mass/higgsCombineExpected${tag}.ProfileLikelihood.mH${mass}.root;
 elif [ $3 = "obs" ]; then
-    echo "combine $workspace -M ProfileLikelihood --significance -m ${mass} -n Observed${tag} | tee observed${tag}.txt";
-          combine $workspace -M ProfileLikelihood --significance -m ${mass} -n Observed${tag} --minimizerAlgo Minuit | tee observed${tag}.txt;
-    mv higgsCombineObserved${tag}.ProfileLikelihood.mH${mass}.root observed${tag}.txt $baseDir/$mass/;
+    echo "combine $workspace -M ProfileLikelihood --significance -m ${mass} -n Observed${tag} --minimizerAlgo Minuit -s ${seed} | tee observed${tag}.txt";
+          combine $workspace -M ProfileLikelihood --significance -m ${mass} -n Observed${tag} --minimizerAlgo Minuit -s ${seed} | tee observed${tag}.txt;
+    mv observed${tag}.txt $baseDir/$mass/;
+    mv higgsCombineObserved${tag}.ProfileLikelihood.mH${mass}.${seed}.root $baseDir/$mass/higgsCombineObserved${tag}.ProfileLikelihood.mH${mass}.root;
+elif [ $3 = "toy" ]; then
+    combine -M HybridNew --frequentist $workspace --significance -m ${mass} -n SigniToy${tag} --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s ${seed} 			      | tee SigniToyA${tag}.txt;
+    combine -M HybridNew --frequentist $workspace --significance -m ${mass} -n SigniToy${tag} --readHybridResult --toysFile=higgsCombineSigniToy${tag}.HybridNew.mH${mass}.${seed}.root --expectedFromGrid=0.5 | tee SigniToyB${tag}.txt;
+    combine -M HybridNew --frequentist $workspace --significance -m ${mass} -n SigniToy${tag} --readHybridResult --toysFile=higgsCombineSigniToy${tag}.HybridNew.mH${mass}.${seed}.root  		 | tee SigniToyC${tag}.txt;
+    mv higgsCombineSigniToy${tag}.HybridNew.mH${mass}.quant0.500.root higgsCombineSigniToy${tag}.HybridNew.mH${mass}.root SigniToy?${tag}.txt $baseDir/$mass/;
 else
     echo "don't know what to do for $3";
 fi
