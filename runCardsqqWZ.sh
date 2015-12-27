3a0138f3c278e3be06e440206b1b2c5842c1f0a6
Guillelmo <ceballos@cern.ch>
2015-12-27 21:54:24 +0100

    new

--- runCardsqqWZ.sh
+++ runCardsqqWZ.sh
@@ -5,7 +5,7 @@ source ~/EVAL_SH66 6_1_1;
 cd ~/releases/CMSSW_6_1_1/src/Combination/;
 
 for mass in 500 900; do
-  for fn in 0 1 2 9 19; do
+  for fn in 0 1 2 9 12 19; do
     cd cards_qqwz/$mass/$fn;
     combineCards.py -S histo_limits_qqwzll_shape_8TeV_Bin*txt > qqwz.text;
     cd -;
