32a1904f990c905f003b6688cd7422ef4b47ea1b
Dylan George Hsu <dghsu@mit.edu>
2017-04-19 16:13:45 -0400

    pipeline for postfit plots :-)

---
+++ runII/postfitYieldsToHistos.C
@@ -0,0 +1,117 @@
+#include <TROOT.h>
+#include <TH1F.h>
+#include <TFile.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+using namespace std;
+const int totalChannels=34;
+const int zzBins=11;
+const int wzBins=11;
+const int zhBins=12;
+const int MVAVarType = 3; const int nBinMVA = 12; Double_t xbins[nBinMVA+1] =  {-2, -1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9}; TString addChan = "3";
+
+void postfitYieldsToHistos(TString postfit_yields="postfit_yields.txt", TString datacard="histo_limits_zll3hinvllnj_sm.text") {
+  // Set up histogram objects
+  TFile *histo_zz_postfit_nice = TFile::Open("histozz_postfit_nice.root", "RECREATE");
+  TH1F *histo_zz[7], *histo_wz[7], *histo_zh[8];
+  for(int i=0; i<7; i++)  histo_zz[i] = new TH1F(Form("histo%d_zz",i), Form("histo%d",i), nBinMVA, xbins);
+  TFile *histo_wz_postfit_nice = TFile::Open("histowz_postfit_nice.root", "RECREATE");
+  for(int i=0; i<7; i++)  histo_wz[i] = new TH1F(Form("histo%d_wz",i), Form("histo%d",i), nBinMVA, xbins);
+  TFile *histo_zh_postfit_nice = TFile::Open("histozh_postfit_nice.root", "RECREATE");
+  for(int i=0; i<8; i++)  histo_zh[i] = new TH1F(Form("histo%d_zh",i), Form("histo%d",i), nBinMVA, xbins);
+
+  // Read in information from text files
+  ifstream postfit_yields_file(postfit_yields.Data());
+  assert(postfit_yields_file.is_open());
+  ifstream datacard_file(datacard.Data());
+  assert(datacard_file.is_open());
+  string line;
+  while( getline(datacard_file, line ) ) {
+    if( line.find("observation") == string::npos ) continue;
+    else {
+      istringstream iss(line);
+      string junk;
+      double dataEvents;
+      iss >> junk;
+      int theChannel=1, theBin;
+      while(iss >> dataEvents) {
+        if(theChannel <= zzBins) {
+          theBin = theChannel+1;
+          histo_zz[0]->SetBinContent(theBin, (double)((int)dataEvents));
+        } else if(theChannel > zzBins && theChannel <= zzBins+wzBins) {
+          theBin = theChannel-zzBins+1;
+          histo_wz[0]->SetBinContent(theBin, (double)((int)dataEvents));
+        } else if(theChannel > zzBins+wzBins && theChannel <= totalChannels) {
+          theBin = theChannel - zzBins - wzBins;
+          histo_zh[0]->SetBinContent(theBin, (double)((int)dataEvents));
+        }
+        theChannel++;
+      }
+      break;
+    }
+  }
+  datacard_file.close();
+  getline( postfit_yields_file, line ); // throwaway line
+  string chstr, process, pm1, pm2, pm3;
+  double N_pre, dN_pre, N_SB, dN_SB, N_B, dN_B;
+  while( getline(postfit_yields_file, line ) ) {
+    // parse postfit yield lines e.g.
+    // ch13 FakeE 2.313 +/-   2.021     1.683 +/-   1.365    1.742 +/-   1.516
+    istringstream iss(line);
+    iss >> chstr >> process >> N_pre >> pm1 >> dN_pre >> N_SB >> pm2 >> dN_SB >> N_B >> dN_B;
+    
+    // Assign the channel
+    chstr = chstr.substr(2);
+    istringstream ich(chstr);
+    int theChannel, theBin, theHisto=-1;
+    ich >> theChannel;
+    bool usePostFit=true;
+
+    // Assign the histo number based on process
+    if(theChannel <= zzBins+wzBins) {
+      if     (process=="EM"   ) theHisto=1;
+      else if(process=="Zg"   ) theHisto=2;
+      else if(process=="WZ"   ) theHisto=3;
+      else if(process=="ZZ"   ) theHisto=4;
+      else if(process=="VVV"  ) theHisto=5;
+      else if(process=="Higgs") theHisto=6;
+    } else if(theChannel <= totalChannels) {
+      if     (process=="EM"       ) theHisto=1;
+      else if(process=="Zjets"    ) theHisto=2;
+      else if(process=="WZ"       ) theHisto=3;
+      else if(process=="ZZ"       ) theHisto=4;
+      else if(process=="VVV"      ) theHisto=5;
+      else if(process=="ZH_hinv"  ) { theHisto=6; usePostFit=false; }
+      else if(process=="ggZH_hinv") { theHisto=7; usePostFit=false; }
+    }
+    if(theHisto<0) continue;
+    // cout << line << std::endl;
+    // cout << theChannel << theHisto << std::endl;
+    // Fill the appropriate histogram with the yield and error
+    if(theChannel <= zzBins) {
+      theBin = theChannel+1;
+      histo_zz[theHisto]->SetBinContent( theBin, N_SB  );
+      histo_zz[theHisto]->SetBinError  ( theBin, dN_SB );
+    } else if(theChannel > zzBins && theChannel <= zzBins+wzBins) {
+      theBin = theChannel-zzBins+1;
+      histo_wz[theHisto]->SetBinContent( theBin, N_SB  );
+      histo_wz[theHisto]->SetBinError  ( theBin, dN_SB );
+    } else if(theChannel > zzBins+wzBins && theChannel <= totalChannels) {
+      theBin = theChannel - zzBins - wzBins;
+      cout << chstr << " " << theChannel << " " << theBin << std::endl;
+      histo_zh[theHisto]->SetBinContent( theBin, usePostFit ? N_SB   : N_pre  );
+      histo_zh[theHisto]->SetBinError  ( theBin, usePostFit ? dN_SB  : dN_pre );
+    }
+  }
+  histo_zz_postfit_nice->cd();
+  for(int i=0; i<7; i++)  histo_zz[i]->Write( Form("histo%d",i) );
+  histo_wz_postfit_nice->cd();
+  for(int i=0; i<7; i++)  histo_wz[i]->Write( Form("histo%d",i) );
+  histo_zh_postfit_nice->cd();
+  for(int i=0; i<8; i++)  histo_zh[i]->Write( Form("histo%d",i) );
+  histo_zz_postfit_nice->Close();
+  histo_wz_postfit_nice->Close();
+  histo_zh_postfit_nice->Close();
+ 
+}
---
+++ runII/runCardsZHCombinations_2016_inc_ordering.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+#grep -e combine -e Observed -e "cted 50" log|awk '{if(NR%3==1)printf("%80s ",$9);if(NR%3!=1)printf(" %6.2f ",$5);if(NR%3==0)printf("\n");}'
+#grep -e combine -e Observed -e "Expected " log|awk '{if(NR%7==1)printf("%80s ",$9);if(NR%7!=1)printf(" %f ",$5);if(NR%7==0)printf("\n");}'
+#grep -e combine -e Observed -e "Expected " log|awk '{if(NR%7==1)printf("%80s ",$9);if(NR%7!=1)printf(" %f ",$5);if(NR%7==0)printf("\n");}' |grep mh|awk '{a=0.8839-0.1227;printf("%6.2f %6.2f %6.2f %6.2f %6.2f %6.2f %6.2f\n",a*$2,a*$5,a*$5,a*$3,a*$4,a*$6,a*$7)}'
+
+#source ~/EVAL_SH66 7_4_7;
+
+#option=$1;
+#
+if  [ $option == 0 ] 
+then
+
+echo "dummy"
+
+else
+#cat `ls cards_inv/330/histo_limits_zz4l3hinvllllnj_shape_13TeV2016_bin*.txt | sort -V` `ls cards_inv/330/histo_limits_wz3l3l_13TeV2016_bin*.txt | sort -V` `ls cards_inv/330/histo_limits_zll3hinvll?j_sm_shape_13TeV2016_bin*.txt | sort -V` histo_limits_zll3hinvllnj_sm.text
+## 0+1-jet
+cd /home/dhsu/cms/cmssw/045/CMSSW_7_4_7/src/Combination/runII/cards_inv/$2;
+rm -rf emptybins
+mkdir emptybins
+for i in `grep "0.00000 \+0.00000 \+0.00000 \+0.00000 \+0.00000 \+0.00000 \+0.00000" histo_limits_zll3hinvll1j_*_shape_13TeV2016_bin*.txt -l`
+do
+  mv $i emptybins
+done
+for i in `grep "0.00000 \+0.00000 \+0.00000 \+0.00000 \+0.00000" histo_limits_zz4l3hinvllll1j_shape_13TeV2016_bin*.txt -l`
+do
+  mv $i emptybins
+done
+for i in `grep "0.00000 \+0.00000 \+0.00000 \+0.00000 \+0.00000 \+0.00000 \+0.00000" histo_limits_wz3l3l_13TeV2016_bin*.txt -l`
+do
+  mv $i emptybins
+done
+mv histo_limits_zz4l3hinvllll1j_shape_13TeV2016_bin0.txt emptybins 2> /dev/null
+mv histo_limits_wz3l3l_13TeV2016_bin0.txt emptybins 2> /dev/null
+#
+ls histo_limits_zll3hinvll1j_*_shape_13TeV2016_bin0.txt -1 > kkk0
+cp kkk0 kkk1
+sed -i 's/_shape_13TeV2016_bin0.txt/*.txt/' kkk0;sed -i 's/zll3hinvll1j/zll3hinvll?j/' kkk0;
+sed -i 's/_shape_13TeV2016_bin0.txt/.text/' kkk1;sed -i 's/zll3hinvll1j/zll3hinvllnj/' kkk1;
+paste kkk0 kkk1 > kkk2
+#awk '{print"combineCards.py -S "$1" > "$2}' kkk2 > kkk3
+#awk '{print"combineCards.py -S histo_limits_zz4l3hinvllll1j_shape_13TeV2016_bin*.txt histo_limits_wz3l3l_13TeV2016_bin*.txt "$1" > "$2}' kkk2 > kkk3
+#chmod a+x kkk3;./kkk3;
+rm kkk3 2> /dev/null
+while read line
+do
+  tokens=( $line )
+  combineCards.py -S `ls histo_limits_zz4l3hinvllll1j_shape_13TeV2016_bin*.txt | sort -V | paste -sd " " -` `ls histo_limits_wz3l3l_13TeV2016_bin*.txt | sort -V | paste -sd " " -` `ls $line | sed -n 's/bin/bin/p' | sort -V | paste -sd " " -` > ${tokens[${#tokens[@]}-1]}
+done < kkk2
+
+ls histo_limits_zll3hinvllnj_*.text > kkk4
+sed -i 's/.text//' kkk4;sed -i 's/zll3hinvll1j/zll3hinvllnj/' kkk4;
+awk '{print"./computeLimit.sh "$1" "'"$2"'" obs /home/dhsu/cms/cmssw/045/CMSSW_7_4_7/src/Combination/runII/cards_inv"}' kkk4 > kkk5
+chmod a+x kkk5;
+cd -
+/home/dhsu/cms/cmssw/045/CMSSW_7_4_7/src/Combination/runII/cards_inv/$2/kkk5;
+
+#rm /home/dhsu/cms/cmssw/045/CMSSW_7_4_7/src/Combination/runII/cards_inv/$2/kkk*;
+fi
