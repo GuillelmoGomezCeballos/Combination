f8242fb4dbf3f36f2eba30de137449204acca702
GuillelmoGomezCeballos <ceballos@cern.ch>
2023-03-18 15:24:41 -0400

    new

--- comb/runPostFit.sh
+++ comb/runPostFit.sh
@@ -10,4 +10,8 @@ cd ~/releases/CMSSW_10_6_27/src/;
 eval `scramv1 runtime -sh`;
 cd -;
 
-PostFitShapesFromWorkspace -w workspace_$1.root -o postfit_$2.root --postfit --print -f fitDiagnostics$1.root:$3
+PostFitShapesFromWorkspace -w workspace_$1.root -o postfit_$2.root \
+-f fitDiagnostics$2.root:$3 --postfit --print 1 --total-shapes 1 --covariance 1
+
+#https://github.com/cms-analysis/CombineHarvester/compare/main...DickyChant:CombineHarvester:sqian_wwg_test_total_cor_total_var
+#/home/submit/ceballos/ana_area/Combination/comb/runPostFit.sh ssww_comb_wwwz ssww_comb_wwwz_obs fit_s
